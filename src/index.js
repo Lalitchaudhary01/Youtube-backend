f1c7aeaeeb8e2c0e7140328a4cc3e7e224333c30
Lalitchdhary01 <Lalitkumar46789@gmail.com>
2024-04-18 17:01:36 +0530

    introduce ROutes

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import dotenv from "dotenv";
 // import mongoose from "mongoose";
 // import { DB_NAME } from "./constants";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config({
   path: "./env ",
@@ -11,8 +12,8 @@ dotenv.config({
 
 connectDB()
   .then(() => {
-    application.listen(process.env.PORT || 8000, () => {
-      console.log(`0 server is running at port : ${process.env.PORT}`);
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(` server is running at port : ${process.env.PORT}`);
     });
   })
   .catch((err) => {
